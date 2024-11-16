1c9b6c04bb2a5df2a88cbd573db8f71670e724ac
Michael Dinerstein <md@missionmike.dev>
2024-11-16 14:42:52 -0800

    Fix: import order.

--- app/components/App.js
+++ app/components/App.js
@@ -1,9 +1,10 @@
-import './css/_App.css';
+import React from 'react';
 
 import Header from './Header';
 import PostList from './PostList';
 import PostSort from './PostSort';
-import React from 'react';
+
+import './css/_App.css';
 
 const App = () => (
   <div className="app">
